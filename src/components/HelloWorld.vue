e3ce996e2a24bc904b06358bdcf3cdb70978c875
deceser <aufhaummtobsc@gmail.com>
2023-03-01 15:11:54 +0200

    initial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const incCount = ref(0);
+const decCoutn = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="incCount++">count is {{ incCount }}</button>
+    <button type="button" @click="decCoutn--">count is {{ decCoutn }}</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
